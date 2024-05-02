921c07d5c24acadeb2ad086807cb9484c328bca1
Mario Panighetti <mpanighetti@mac.com>
2024-05-01 19:42:12 -0700

    switched to POSIX Shell
    - switched to POSIX Shell
    - corrected working directory to `/private/tmp/AutoPkg Cache Cleaner`
    - improved variable substring protection
    - improved emoji representation

--- AutoPkg Cache Cleaner.sh
+++ AutoPkg Cache Cleaner.sh
@@ -1,16 +1,14 @@
-#!/bin/zsh
+#!/bin/sh
 
 ###
 #
 #            Name:  AutoPkg Cache Cleaner.sh
-#     Description:  For each AutoPkg recipe cache, searches for and deletes all
-#                   downloaded resources and compiled packages with creation
-#                   dates older than the specified number of days.
+#     Description:  For each AutoPkg recipe cache, this script searches for and deletes all downloaded resources and compiled packages with creation dates older than the specified number of days.
 #                   https://github.com/mpanighetti/autopkg-cache-cleaner
 #          Author:  Mario Panighetti
 #         Created:  2017-09-13
-#   Last Modified:  2020-04-03
-#         Version:  1.2
+#   Last Modified:  2024-05-01
+#         Version:  1.3
 #
 ###
 
@@ -23,12 +21,12 @@
 # Leave these values as-is.
 scriptName=$(basename "$0")
 loggedInUser=$(/usr/bin/stat -f%Su "/dev/console")
-loggedInUserHome=$(/usr/bin/dscl . -read "/Users/$loggedInUser" NFSHomeDirectory | /usr/bin/awk '{print $NF}')
-loggedInUserLibrary="$loggedInUserHome/Library"
-autopkgPrefs="$loggedInUserLibrary/Preferences/com.github.autopkg"
+loggedInUserHome=$(/usr/bin/dscl . -read "/Users/${loggedInUser}" NFSHomeDirectory | /usr/bin/awk '{print $NF}')
+loggedInUserLibrary="${loggedInUserHome}/Library"
+autopkgPrefs="${loggedInUserLibrary}/Preferences/com.github.autopkg"
 autopkgCacheDir=$(/usr/bin/defaults read "$autopkgPrefs" CACHE_DIR 2> "/dev/null")
-workingDir="/tmp/AutoPkgCacheCleaner"
-scriptLog="$loggedInUserLibrary/Logs/AutoPkg Cache Cleaner.log"
+workingDir="/private/tmp/AutoPkg Cache Cleaner"
+scriptLog="${loggedInUserLibrary}/Logs/AutoPkg Cache Cleaner.log"
 defaultCutoff="30"
 
 
@@ -42,52 +40,56 @@ if [ -e "$scriptLog" ]; then
   /bin/rm "$scriptLog"
 fi
 touch "$scriptLog"
-exec > >(/usr/bin/tee -a "$scriptLog") 2>&1
+exec 1>>"$scriptLog" 2>&1
 
 
 # Start script.
-/bin/echo "Running $scriptName..."
-/bin/echo "$scriptName start timestamp: $(/bin/date)"
+echo "Running ${scriptName}..."
+echo "${scriptName} start timestamp: $(/bin/date)"
 
 
 # Check desired cutoff for aging files in days (default to 30).
 if [ -n "$1" ]; then
-  if [[ "$1" =~ ^[0-9]+$ ]]; then
+  integerTest=$(echo "${1}" | /usr/bin/tr -d "[:digit:]")
+  if [ -z "$integerTest" ]; then
     oldCutoff="$1"
   else
-    /bin/echo "Invalid input: \"$1\", using script default."
+    echo "Invalid input: \"${1}\", using script default."
     oldCutoff="$defaultCutoff"
   fi
 else
   oldCutoff="$defaultCutoff"
 fi
-/bin/echo "Cutoff for aging files: $oldCutoff days"
+echo "Cutoff for aging files: ${oldCutoff} days"
 
 
 # If CACHE_DIR is not defined in plist, use default path.
 if [ -z "$autopkgCacheDir" ]; then
-  autopkgCacheDir="$loggedInUserLibrary/AutoPkg/Cache"
-  /bin/echo "AutoPkg cache directory not defined, using default path ($autopkgCacheDir)."
+  autopkgCacheDir="${loggedInUserLibrary}/AutoPkg/Cache"
+  echo "AutoPkg cache directory not defined, using default path: ${autopkgCacheDir}"
 fi
 
 
 # Exit if CACHE_DIR does not exist.
 if [ ! -d "$autopkgCacheDir" ]; then
-  /bin/echo "❌ ERROR: AutoPkg cache directory not found at $autopkgCacheDir, unable to proceed."
+  echo "❌ ERROR: AutoPkg cache directory not found at ${autopkgCacheDir}, unable to proceed."
   exit 1
 else
-  /bin/echo "AutoPkg cache directory found at $autopkgCacheDir."
+  echo "AutoPkg cache directory found: ${autopkgCacheDir}"
 fi
 
 
 # Initialize working directory for temp files.
-if [ ! -d "$workingDir" ]; then
-  /bin/mkdir -p "$workingDir"
+if [ -d "$workingDir" ]; then
+  /bin/rm -rf "$workingDir"
+  echo "Deleted existing working directory."
 fi
+/bin/mkdir -p "$workingDir"
+echo "Working directory created: ${workingDir}"
 
 
 # Initialize deleted files list.
-deleteTheseFilesPath="$workingDir/delete-these-files"
+deleteTheseFilesPath="${workingDir}/delete-these-files"
 if [ -e "$deleteTheseFilesPath" ]; then
   /bin/rm "$deleteTheseFilesPath"
 fi
@@ -95,12 +97,12 @@ touch "$deleteTheseFilesPath"
 
 
 # Initialize math file for space-saving calculation.
-mathsFilePath="$workingDir/maths"
+mathsFilePath="${workingDir}/maths"
 if [ -e "$mathsFilePath" ]; then
   /bin/rm "$mathsFilePath"
 fi
 touch "$mathsFilePath"
-/bin/echo "0" > "$mathsFilePath"
+echo "0" > "$mathsFilePath"
 
 
 # Search CACHE_DIR downloads folders for aging files.
@@ -117,13 +119,11 @@ find "$autopkgCacheDir" -maxdepth 2 -mtime +"$oldCutoff" -name "*.pkg" >> "$dele
 # Delete files, collecting byte size beforehand for space-saving calculation.
 deleteTheseFiles=$(/bin/cat "$deleteTheseFilesPath")
 if [ -z "$deleteTheseFiles" ]; then
-  /bin/echo "No files found greater than $oldCutoff days old, no action required."
+  echo "No files found greater than ${oldCutoff} days old, no action required."
   exit 0
 else
-  /bin/echo ""
-  /bin/echo "Removing files:"
-  /bin/echo ""
-  /bin/echo "$deleteTheseFiles" | while read -r deleteMe; do
+  echo "🧹 Removing files..."
+  echo "$deleteTheseFiles" | while read -r deleteMe; do
     /bin/ls -l "$deleteMe" | /usr/bin/awk '{print $5}' >> "$mathsFilePath"
     /bin/rm -rv "$deleteMe"
   done
@@ -140,19 +140,17 @@ if [ "$oldCacheSizeTotal" -eq 0 ]; then
 elif [ "$oldCacheSizeTotal" -gt 1023 ]; then
   if [ "$oldCacheSizeTotal" -gt 1048575 ]; then
     if [ "$oldCacheSizeTotal" -gt 1073741823 ]; then
-      oldCacheSizeLCD=$(/bin/echo "$oldCacheSizeTotal" | /usr/bin/awk '{sizeGB = $1 / 1024 / 1024 / 1024 ; print sizeGB " GB" }')
+      oldCacheSizeLCD=$(echo "$oldCacheSizeTotal" | /usr/bin/awk '{sizeGB = $1 / 1024 / 1024 / 1024 ; print sizeGB " GB" }')
     else
-      oldCacheSizeLCD=$(/bin/echo "$oldCacheSizeTotal" | /usr/bin/awk '{sizeMB = $1 / 1024 / 1024 ; print sizeMB " MB" }')
+      oldCacheSizeLCD=$(echo "$oldCacheSizeTotal" | /usr/bin/awk '{sizeMB = $1 / 1024 / 1024 ; print sizeMB " MB" }')
     fi
   else
-    oldCacheSizeLCD=$(/bin/echo "$oldCacheSizeTotal" | /usr/bin/awk '{sizeKB = $1 / 1024 ; print sizeKB " KB" }')
+    oldCacheSizeLCD=$(echo "$oldCacheSizeTotal" | /usr/bin/awk '{sizeKB = $1 / 1024 ; print sizeKB " KB" }')
   fi
 else
-  oldCacheSizeLCD=$(/bin/echo "$oldCacheSizeTotal B")
+  oldCacheSizeLCD="${oldCacheSizeTotal} B"
 fi
-/bin/echo ""
-/bin/echo "Space saved: $oldCacheSizeLCD"
-/bin/echo ""
+echo "✅ All clean! Space saved: ${oldCacheSizeLCD}"
 
 
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # AutoPkg Cache Cleaner
-For each AutoPkg recipe cache, searches for and deletes all downloaded resources and compiled packages with creation dates older than the specified number of days.
+For each AutoPkg recipe cache, this script searches for and deletes all downloaded resources and compiled packages with creation dates older than the specified number of days.
 
 This script expects to run in user context (the same user account running AutoPkg) and does not require `sudo` for a typical AutoPkg install.
